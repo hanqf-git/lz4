0951ad011032c87c178f320a18e95ed312b79a57
hanqf-git <qiufeng.han@intel.com>
2022-05-10 22:02:30 +0800

    Set LZ4_DISTANCE_MAX  to 32767 - Windown size 32KB

--- lib/lz4.h
+++ lib/lz4.h
@@ -564,7 +564,7 @@ LZ4LIB_STATIC_API void LZ4_attach_dictionary(LZ4_stream_t* workingStream, const 
 #define LZ4_DECOMPRESS_INPLACE_MARGIN(compressedSize)          (((compressedSize) >> 8) + 32)
 #define LZ4_DECOMPRESS_INPLACE_BUFFER_SIZE(decompressedSize)   ((decompressedSize) + LZ4_DECOMPRESS_INPLACE_MARGIN(decompressedSize))  /**< note: presumes that compressedSize < decompressedSize. note2: margin is overestimated a bit, since it could use compressedSize instead */
 
-#define LZ4_DISTANCE_MAX   32768  /*Set maximum history window size to 32K*/
+#define LZ4_DISTANCE_MAX   32767  /*Set maximum history window size to 32K*/
 #ifndef LZ4_DISTANCE_MAX   /* history window size; can be user-defined at compile time */
 #  define LZ4_DISTANCE_MAX 65535   /* set to maximum value by default */
 #endif
